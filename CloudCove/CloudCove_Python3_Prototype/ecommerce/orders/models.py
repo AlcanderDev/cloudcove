f62dd0c774ca6fcfea566794cc70af627c3dee57
xlcy32x <xlcy32x@gmail.com>
2018-07-04 02:34:04 +0800

    Added Random Generated OrderID

--- CloudCove/CloudCove_Python3_Prototype/ecommerce/orders/models.py
+++ CloudCove/CloudCove_Python3_Prototype/ecommerce/orders/models.py
@@ -1,8 +1,26 @@
 from django.db import models
 from shop.models import Product
+from django.db.models.signals import pre_save
+import random
+import string
+
+def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+def unique_order_id_generator(instance):
+    order_new_id= random_string_generator()
+
+    Klass= instance.__class__
+
+    qs_exists= Klass.objects.filter(order_id= order_new_id).exists()
+    if qs_exists:
+        return unique_order_id_generator(instance)
+    return order_new_id
 
 
 class Order(models.Model):
+    order_id = models.CharField(max_length=120, blank= True)
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
     email = models.EmailField()
@@ -17,10 +35,17 @@ class Order(models.Model):
         ordering = ('-created', )
 
     def __str__(self):
-        return 'Order {}'.format(self.id)
+        return 'Order {}'.format(self.order_id)
 
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
+        
+def pre_save_create_order_id(sender, instance, *args, **kwargs):
+    if not instance.order_id:
+        instance.order_id= unique_order_id_generator(instance)
+
+
+pre_save.connect(pre_save_create_order_id, sender=Order)
 
 
 class OrderItem(models.Model):
